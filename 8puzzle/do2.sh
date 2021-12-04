b346aef525db30c3f7b96b3175b9cd355e5e282c
Ruo Ando <ando.ruo@gmail.com>
2021-12-05 00:39:05 +0900

    plot

--- 8puzzle/do2.sh
+++ 8puzzle/do2.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+rm -rf result-do2
+touch result-do2
+
+rm -rf result-do2-plot
+touch result-do2-plot
+
+rm -rf result-do2-plot3
+touch result-do2-plot3
+
+echo "bidirectional,vertical,horizontal" >> result-do2-plot3
+echo "vertical,horizontal" >> result-do2-plot
+
 while read line
 do
     r=`echo $line  | cut -d ',' -f 2`
@@ -15,5 +27,10 @@ do
 
     tmp=`echo $tail_v","$tail_h | tr -d " "`
     echo $tmp,$line
+    echo $tmp,$line >> result-do2
+
+    first=`echo $line | cut -d "," -f 1`
+    echo $tail_v","$tail_h | tr -d " " >> result-do2-plot
+    echo $first","$tail_v","$tail_h | tr -d " " >> result-do2-plot3
     
 done < $1 
