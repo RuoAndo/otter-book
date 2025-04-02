dffec0afdd71bd2394b6cf613423ec638dee9fcc
root <root@DESKTOP-329HOL1.>
2025-04-02 12:24:08 +0900

    sh

--- rubic/run_otter.sh
+++ rubic/run_otter.sh
@@ -5,8 +5,11 @@ counter=1
 while true; do
     echo "[$counter 回目の実行]"
 
+	python3 genrand2.py > tmp
+	cat head.txt tmp tail.txt > ${counter}.in
+
     # 30秒以内に ./otter が終了しなければ強制終了（終了コード 124）
-    timeout 10 ./otter < 1
+    timeout 10 ./otter < ${counter}.in
 
     if [ $? -eq 124 ]; then
         echo "10秒経過したため強制終了しました。"
