58e7e0041abf0da8bf26e6062f33b3242952c442
Ruo Ando <ando.ruo@gmail.com>
2025-04-25 08:38:43 +0900

    l

---
+++ liar/1.sh
@@ -0,0 +1,23 @@
+echo "---"
+/root/otter-3.3f/bin/otter < liar.in | grep hyper | grep A | grep G
+
+echo "---"
+/root/otter-3.3f/bin/otter < liar.in | grep hyper | grep A | grep I
+
+echo "---"
+/root/otter-3.3f/bin/otter < liar.in | grep hyper | grep B | grep G
+
+echo "---"
+/root/otter-3.3f/bin/otter < liar.in | grep hyper | grep B | grep I
+
+echo "---"
+/root/otter-3.3f/bin/otter < liar.in | grep hyper | grep C | grep G
+
+echo "---"
+/root/otter-3.3f/bin/otter < liar.in | grep hyper | grep C | grep I
+
+echo "---"
+/root/otter-3.3f/bin/otter < liar.in | grep hyper | grep D | grep G
+
+echo "---"
+/root/otter-3.3f/bin/otter < liar.in | grep hyper | grep D | grep I
