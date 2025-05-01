f9d25f977d96b2ea2ee50e7d5d2eceb0dfa2e40e
Ruo Ando <ando.ruo@gmail.com>
2025-05-01 21:12:34 +0900

    3

--- liar/test3.sh
+++ liar/test3.sh
@@ -10,6 +10,7 @@ while read line; do
 
     # zenhan と kouhan の内容を tmp2 に結合
     cat zenhan > tmp2
+    echo "" >> tmp2
     echo "$line" >> tmp2
     echo "" >> tmp2
     cat kouhan >> tmp2
@@ -18,7 +19,8 @@ while read line; do
     echo "$line" > sos.${count}
 
     # otter を実行してログを生成
-    /root/otter-3.3f/bin/otter < tmp2 | tee log.${count}
+    #/root/otter-3.3f/bin/otter < tmp2 | tee log.${count}
+    /root/otter-3.3f/bin/otter < tmp2 > log.${count}
 
     # "clauses generated" の行を result に追加
     r=`grep "clauses generated" log.${count}`
@@ -33,7 +35,18 @@ done < list-sos-3
 for ((i=1; i<=count-1; i++)); do
     echo "Processing log.${i}..."    
     # log.${i} に対して指定された grep コマンドを実行
-    grep "hyper" log.${i} | grep -E '^[0-9]+ \[.*?\] P\((I|G)\([A-Z]\)\)\.' 
-    cat r.${i}
-    cat sos.${i}
+    # grep "hyper" log.${i} | grep -E '^[0-9]+ \[.*?\] P\((I|G)\([A-Z]\)\)\.' 
+
+    #cat log.${i}
+    #grep "hyper" log.${i} | sed -n 's/.*\] \(P([IGT]([A-Z]))\)\./\1/p' 
+    #grep "hyper" log.${i} | sed -n 's/.*\] \(P([T]([A-Z]))\)\./\1/p' 
+    #grep "hyper" log.${i} | sed -n 's/.*\] \(P([T]([A-Z]))\)\./\1/p' | sort | uniq 
+    g1=`grep "hyper" log.${i} | sed -n 's/.*\] \(P([T]([A-Z]))\)\./\1/p' | awk '!seen[$0]++' | paste -sd,`
+
+    r1=`cat r.${i} | cut -d " " -f3`
+    r2=`cat sos.${i}`
+
+    echo $r2","$r1","$g1
+
+
 done
